cfb546fe02c2d579cb65960d277bbcfbd5047349
MARSRU <zrw1215225@163.com>
2017-12-05 10:59:30 +0800

    第一次

---
+++ one.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/5
+ * Time: 9:21
+ */
+
+$a = 1;
+
+for ($int = 1; $i < 100; $i++) {
+
+
+    echo "###########";
+
+
+ }
+
+
